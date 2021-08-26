7bb4b9e75c2a070df28c2dbddd6a996168c1b8cd
Afdhal_FA <fuad.afdal.fa@example.com>
2021-08-26 16:08:43 +0700

    Make signOut

--- lib/ui/pages/setting_page.dart
+++ lib/ui/pages/setting_page.dart
@@ -1,17 +1,44 @@
+import 'package:airplan/cubit/auth_cubit.dart';
+import 'package:airplan/shared/theme.dart';
 import 'package:airplan/ui/widgets/custom_button.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomButton(
-        title: 'Sign Out',
-        onPressed: () {},
-        width: 220,
-      ),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthFailed) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.error),
+              backgroundColor: kRedColor,
+            ),
+          );
+        } else if (state is AuthInitial) {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/sign-up', (route) => false);
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return Center(
+          child: CustomButton(
+            title: 'Sign Out',
+            onPressed: () {
+              context.read<AuthCubit>().signOut();
+            },
+            width: 220,
+          ),
+        );
+      },
     );
   }
 }
